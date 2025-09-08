61f49d24bb723a917103c8389893df71b9af902b
Vijay Kalyan Yandrapu <161374046+vijaykalyanyandrapu@users.noreply.github.com>
2025-09-08 17:23:08 +0530

    Create tasl-2a.java

---
+++ tasl-2a.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class IntervalListIntersections {
+    public static int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < firstList.length && j < secondList.length) {
+            int start = Math.max(firstList[i][0], secondList[j][0]);
+            int end = Math.min(firstList[i][1], secondList[j][1]);
+
+            if (start <= end) {
+                result.add(new int[] {start, end});
+            }
+
+            // Move pointer of interval that ends first
+            if (firstList[i][1] < secondList[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] firstList = {{0, 2}, {5, 10}, {13, 23}, {24, 25}};
+        int[][] secondList = {{1, 5}, {8, 12}, {15, 24}, {25, 26}};
+
+        int[][] intersections = intervalIntersection(firstList, secondList);
+
+        System.out.println("Intersections:");
+        for (int[] interval : intersections) {
+            System.out.println(Arrays.toString(interval));
+        }
+    }
+}
