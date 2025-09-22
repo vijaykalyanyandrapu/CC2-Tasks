4015228241a58e47df36a94d40b952c9379c1c75
Vijay Kalyan Yandrapu <161374046+vijaykalyanyandrapu@users.noreply.github.com>
2025-09-22 16:03:21 +0530

    Create task-5c.java

---
+++ task-5c.java
@@ -0,0 +1,67 @@
+class RemoveElementLinkedList {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public void remove(int key) {
+        if (head == null) return;
+
+        if (head.data == key) {
+            head = head.next;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null && temp.next.data != key) {
+            temp = temp.next;
+        }
+
+        if (temp.next != null) {
+            temp.next = temp.next.next;
+        }
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        RemoveElementLinkedList list = new RemoveElementLinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+
+        System.out.println("Original List:");
+        list.display();
+
+        list.remove(30);
+        System.out.println("After removing 30:");
+        list.display();
+    }
+}
