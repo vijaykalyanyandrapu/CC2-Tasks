7e37915ff77aa0a9d540568e3f5aca4e411633cc
Vijay Kalyan Yandrapu <161374046+vijaykalyanyandrapu@users.noreply.github.com>
2025-09-22 16:03:57 +0530

    Create task-6a.java

---
+++ task-6a.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+public class CountingSort {
+ public static void countingSort(int[] arr) {
+ int max = Arrays.stream(arr).max().getAsInt();
+ int min = Arrays.stream(arr).min().getAsInt();
+ int range = max - min + 1;
+ int[] count = new int[range];
+ int[] output = new int[arr.length];
+ for (int i = 0; i < arr.length; i++) {
+ count[arr[i] - min]++;
+ }
+ for (int i = 1; i < count.length; i++) {
+ count[i] += count[i - 1];
+ }
+ for (int i = arr.length - 1; i >= 0; i--) {
+ output[count[arr[i] - min] - 1] = arr[i];
+ count[arr[i] - min]--;}
+ for (int i = 0; i < arr.length; i++) {
+ arr[i] = output[i];
+ }}
+ public static void main(String[] args) {
+ int[] arr = {4, 2, 2, 8, 3, 3, 1};
+ System.out.println("Original Array: " + Arrays.toString(arr));
+ countingSort(arr);
+ System.out.println("Sorted Array: " + Arrays.toString(arr));
+ }}
